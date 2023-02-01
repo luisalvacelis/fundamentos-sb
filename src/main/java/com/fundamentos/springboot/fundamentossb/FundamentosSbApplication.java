0a184269a8b43623d7a2f890dd8d546ca458fbb0
Luis Alva Celis <40306835+luisalvacelis@users.noreply.github.com>
2023-02-01 12:26:48 -0500

    first commit

---
+++ src/main/java/com/fundamentos/springboot/fundamentossb/FundamentosSbApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.fundamentossb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentos/springboot/fundamentossb/FundamentosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.fundamentossb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
